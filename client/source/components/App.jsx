98bb2f4d8bda01e62c3532f5367afb367e215886
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-02-28 10:31:06 -0800

    Set up server index.js file and App.jsx file to test page load

---
+++ client/source/components/App.jsx
@@ -0,0 +1,12 @@
+// LIBRARY IMPORTS
+import React from 'react';
+
+// LOCAL IMPORTS
+
+function App() {
+  return (
+    <div>1More Page</div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,24 @@
+// LIBRARY IMPORTS
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+// const cors = require('cors');
+
+// LOCAL IMPORTS
+const router = require('./router');
+
+// CREATE EXPRESS SERVER INSTANCE
+const app = express();
+
+// USE MIDDLEWARE
+app.use(express.json());
+// app.use(cors());
+
+// SERVE UP STATIC FILES
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use('', router);
+
+const { HOST, PORT } = process.env;
+app.listen(PORT);
+console.log(`Server listening at http://${HOST}:${PORT}`);
