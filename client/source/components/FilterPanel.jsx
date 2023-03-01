0b04aa6adb3134836d6d1b340de2c033984f9c70
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-03-01 14:58:43 -0800

    Added basic styling and font family

---
+++ client/source/components/FilterPanel.jsx
@@ -0,0 +1,13 @@
+// LIBRARY IMPORTS
+import React, { useState, useEffect } from 'react';
+
+// LOCAL IMPORTS
+function FilterPanel() {
+  return (
+    <div>
+      <div className="input-group" />
+    </div>
+  );
+}
+
+export default FilterPanel;
---
+++ client/source/components/styles/App.styled.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  .home {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  .home_panel_wrapper,
+  .home_list_wrapper {
+    overflow-y: auto;
+  }
+
+  .home_panel-list_wrapper {
+    display: flex;
+    flex: 1;
+    height: 100vh;
+  }
+
+  .home_panel_wrapper {
+    background: #D4A373;
+    flex-basis: 18rem;
+    height: 100vh;
+  }
+
+  .home_list_wrapper {
+    background: #CCD5AE;
+    flex: 1;
+    height: 100vh;
+  }
+`;
+
+export default StyledApp;
